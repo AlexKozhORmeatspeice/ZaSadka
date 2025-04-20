46594c5dcaf10921e8edd00d1df383e3c5475436
meatspeice <54778699+AlexKozhORmeatspeice@users.noreply.github.com>
2025-04-20 20:34:19 +0300

    Fixed last commit

--- Core/Scenes/MainMenu/sprite_2d.gd
+++ Core/Scenes/MainMenu/sprite_2d.gd
@@ -7,15 +7,15 @@ const bag_chance = 0.3
 
 
 func _ready():
-	position = Vector2(get_viewport_rect().size.x * 0.6,  -texture.get_height() / 2)
-	conv2.position = Vector2(get_viewport_rect().size.x * 0.6,  texture.get_height() / 2)
+	position = Vector2(get_viewport_rect().size.x * 0.6,  -(float)(texture.get_height()) / 2.0)
+	conv2.position = Vector2(get_viewport_rect().size.x * 0.6,  (float)(texture.get_height()) / 2.0)
 	
 #fuck yeah simulator
 func _process(delta) -> void:
 	position.y += conv_speed * delta
 	
-	if conv2.position.y >  get_viewport_rect().size.y + texture.get_height() / 2:
-		conv2.position = Vector2(get_viewport_rect().size.x * 0.6,  -texture.get_height() / 2)
+	if conv2.position.y >  get_viewport_rect().size.y + (float)(texture.get_height()) / 2.0:
+		conv2.position = Vector2(get_viewport_rect().size.x * 0.6,  -(float)(texture.get_height()) / 2.0)
 		
 	if position.y > get_viewport_rect().size.y + texture.get_height() / 2:
-		position.y = -texture.get_height() / 2
+		position.y = -(float)(texture.get_height()) / 2.0
